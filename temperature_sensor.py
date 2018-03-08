c5585297d1bad906b6e1bcedbbda1b8943a6bec6
dreweastep <dreweastep@gmail.com>
2018-03-08 15:13:51 -0600

    first commit

---
+++ temperature_sensor.py
@@ -0,0 +1,88 @@
+# temperature_sensor.py
+#
+# Created by Drew Eastep
+#
+# Update log:
+#
+# 10/23/17 -- Created file
+#
+# 11/06/2017 -- Copied device serial number
+#
+# 11/13/2017 -- Adding code for second sensor and edited data
+#
+# 11/27/2017 -- Added serial code for second sensor
+#
+# 01/22/2018 -- Added SMTP email alerts
+#
+# 02/08/2018 -- Changed SMTP to send from internal dunwoody mail sensor with fake address.
+
+# Recording temperature using ds18b20 sensor
+
+import os
+import time
+import smtplib
+
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+temp_sensor_1 = '/sys/bus/w1/devices/28-031722a436ff/w1_slave'
+temp_sensor_2 = '/sys/bus/w1/devices/28-03172293f1ff/w1_slave'
+
+SERVER = "braincoral.dunwoody.tec.mn.us"
+
+FROM = 'opensourcepialerts@example.com'
+
+TO = ['cgabrielson@dunwoody.edu', 'easandb@dunwoody.edu', 'smekylg@dunwoody.edu']
+
+def temp_raw(temp_sensor):
+
+    f = open(temp_sensor, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp(temp_sensor):
+
+    lines = temp_raw(temp_sensor)
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = temp_raw(temp_sensor)
+
+    temp_output = lines[1].find('t=')
+
+    if temp_output != -1:
+        temp_string = lines[1].strip()[temp_output + 2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return round(temp_c, 1), round(temp_f, 1)
+
+
+while True:
+    print('Sensor 1:', read_temp(temp_sensor_1))
+    print('Sensor 2:', read_temp(temp_sensor_2))
+    print('')
+
+    if read_temp(temp_sensor_1)[1] > 90 or read_temp(temp_sensor_2)[1] > 90:
+        MESSAGE = "The raspberry pi temperature monitor is reading a temperature of" \
+                  + str(read_temp(temp_sensor_1)[1]) + "degrees fahrenheit."
+
+        server = smtplib.SMTP('SERVER')
+        server.sendmail(FROM, TO, MESSAGE)
+        server.quit()
+
+        time.sleep(300)
+
+    if read_temp(temp_sensor_1)[1] < 42 or read_temp(temp_sensor_2)[1] < 42:
+        MESSAGE = "The raspberry pi temperature monitor is reading a temperature of" \
+                  + str(read_temp(temp_sensor_1)[1]) + "degrees fahrenheit."
+
+        server = smtplib.SMTP(SERVER)
+        server.sendmail(FROM, TO, MESSAGE)
+        server.quit()
+
+        time.sleep(300)
+
+        time.sleep(5)
+
