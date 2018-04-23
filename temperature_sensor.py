4b5df5fbeacafa423fe3fe39adf3f2deff948778
dreweastep <dreweastep@gmail.com>
2018-04-23 12:52:25 -0500

    Added proper email formatting using MIME extension

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -15,13 +15,19 @@
 # 01/22/2018 -- Added SMTP email alerts
 #
 # 02/08/2018 -- Changed SMTP to send from internal dunwoody mail sensor with fake address.
+#
+# 03/22/2018 -- Code now takes in multiple values and sends emails only if multiple high readings are read.
+#
+# 03/29/2018 -- Fixed logical and syntax bugs
+#
+# 04/23/2018 -- Added MIME extension to properly format email
 
 # Recording temperature using ds18b20 sensor
 
 import os
 import time
 import smtplib
-
+from email.mime.text import MIMEText
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -30,10 +36,10 @@ temp_sensor_1 = '/sys/bus/w1/devices/28-031722a436ff/w1_slave'
 temp_sensor_2 = '/sys/bus/w1/devices/28-03172293f1ff/w1_slave'
 
 SERVER = "braincoral.dunwoody.tec.mn.us"
-
 FROM = 'opensourcepialerts@example.com'
-
+SUBJECT = "Play.Dunwoody Server"
 TO = ['cgabrielson@dunwoody.edu', 'easandb@dunwoody.edu', 'smekylg@dunwoody.edu']
+
 
 def temp_raw(temp_sensor):
 
@@ -41,6 +47,16 @@ def temp_raw(temp_sensor):
     lines = f.readlines()
     f.close()
     return lines
+
+
+def send_mail(message):
+
+    MESSAGE = MIMEText(message)
+    MESSAGE['Subject'] = SUBJECT
+
+    server = smtplib.SMTP(SERVER)
+    server.sendmail(FROM, TO, MESSAGE.as_string())
+    server.quit()
 
 
 def read_temp(temp_sensor):
@@ -57,6 +73,7 @@ def read_temp(temp_sensor):
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return round(temp_c, 1), round(temp_f, 1)
+
 
 temp_values = []
 for i in range(0, 20):
@@ -76,21 +93,19 @@ for each in temp_values:
     if each < 42:
         cold_readings = cold_readings + 1
 
-    if hot_readings > 5:
-        MESSAGE = "The raspberry pi temperature monitor is reading a temperature of" \
-                  + str(read_temp(temp_sensor_1)[1]) + "degrees fahrenheit."
+if hot_readings > 5:
+    MESSAGE = "The raspberry pi temperature monitor is reading an average temperature of" \
+              + str(sum(temp_values)/len(temp_values)) + "degrees fahrenheit."
 
-        server = smtplib.SMTP('SERVER')
-        server.sendmail(FROM, TO, MESSAGE)
-        server.quit()
+    send_mail(MESSAGE)
 
-    if cold_readings > 5:
-        MESSAGE = "The raspberry pi temperature monitor is reading a temperature of" \
-                  + str(read_temp(temp_sensor_1)[1]) + "degrees fahrenheit."
 
-        server = smtplib.SMTP(SERVER)
-        server.sendmail(FROM, TO, MESSAGE)
-        server.quit()
+if cold_readings > 5:
+    MESSAGE = "The raspberry pi temperature monitor is reading an average temperature of" \
+              + str(sum(temp_values)/len(temp_values)) + "degrees fahrenheit."
+
+    send_mail(MESSAGE)
+
 
 
 
